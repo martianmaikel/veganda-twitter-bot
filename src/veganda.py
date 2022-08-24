3b6213d69bc8b1b8b946968e837acc12ed548aee
martianmaikel <maikellive@freenet.de>
2022-08-24 14:50:54 +0200

    Setup tweepy and Test Tweet

--- src/veganda.py
+++ src/veganda.py
@@ -1,1 +1,14 @@
-import ../config
+# api token
+import config
+
+# imports
+import tweepy
+
+tweet = "Hello World"
+
+# api
+auth = tweepy.OAuthHandler(config.API_KEY, config.API_SECRET)
+auth.set_access_token(config.ACCESS_TOKEN, config.ACCESS_TOKEN_SEC)
+api = tweepy.API(auth)
+
+api.create_friendship('martianmaikel')
